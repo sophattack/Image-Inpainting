226e5240edec8e3f53d62a3630faf757102a2816
Diadem Shoukralla <diadem.shoukralla@mail.utoronto.ca>
2020-11-16 16:42:54 -0500

    added untared data to data file but also ignored in git

--- playground.py
+++ playground.py
@@ -1,1 +1,11 @@
-# Python file to test/play around with code
+import tarfile
+
+def untar_data(name, outdir='./data'):
+    my_tar = tarfile.open('./Indoor-scene-recognition/'+name)
+    my_tar.extractall(outdir)
+    my_tar.close()
+
+
+# Uncomment to untar data
+# untar_data("indoorCVPR_09annotations.tar")
+# untar_data("indoorCVPR_09.tar")
